71242db5c0ba648c0810f49aae6c4087a50d9382
IfKaioTavares <20222TADSSAJ0032@ifba.edu.br>
2023-10-31 18:27:41 -0300

    correcao lista estatica

---
+++ 4-ListaEstatica/q1Corrigida.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#define MaxLength 100
+#define VetorStart 0
+
+typedef struct{
+    int value;
+}Data;
+
+typedef int Pointer;
+
+typedef struct {
+    Data data[MaxLength];
+    Pointer first, last;
+}StaticList;
+
+void StartList(StaticList *List){
+    List->first = VetorStart;
+    List->last = List->first;
+};
+
+int empetyList(StaticList *List){
+    return List->first == List->last;
+}
+
+void addData(StaticList *List, Data data){
+    if(List->last == MaxLength -1){
+        printf("Lista cheia\n");
+    }else{
+        List->data[List->last] = data;
+        List->last++;
+    }
+}
+
+void showList(StaticList *List){
+    if(empetyList(List)){
+        printf("Lista vazia\n");
+    }else{
+        printf("List itens:\n");
+        for(int i = VetorStart; i < List->last; i++){
+            printf("%d: %d\n", i+1, List->data[i].value);
+        }
+    }
+};
+
+void addData_inPosition(StaticList *List, Data data, Pointer pos){
+    if(List->last == MaxLength - 1){
+        printf("Lista cheia\n");
+    }else if (pos < 0 || pos > List->last )
+    {
+        printf("Posição invalida");
+    }else{
+        //Deslocar os itens da lista
+        for(int i = List->last; i > pos; i--){
+            List->data[i] = List->data[i-1];
+        }
+        List->data[pos] = data;
+        List->last++;
+    }
+}
+
+void checkOrder(StaticList *List){
+    int increasing = 0;
+    int decreasing = 0;
+    for(int i = List->first + 1; i < List->last; i++){
+        if(List->data[i - 1].value < List->data[i].value){
+            increasing = 1;
+        }
+        if(List->data[i-1].value > List->data[i].value){
+            decreasing = 1;
+        }
+    }
+
+    if(increasing && decreasing){
+        printf("Lista nao ordernada\n");
+    }else if (increasing && !decreasing)
+    {
+        printf("Lista crescente\n");
+    }else if (!increasing && decreasing)
+    {
+        printf("Lista decrescente\n");
+    }else{
+        printf("Lista com dados iguais\n");
+    }
+}
+
+void compareLists(StaticList *List1, StaticList *List2){
+    int validation = 0;
+    for(int i = VetorStart; i<List1->last; i++){
+        if(List1->data[i].value != List2->data[i].value){
+            validation = 1;
+        }
+    }
+
+    if(validation || (List1->last != List2->last)){
+        printf("Listas diferentes");
+    }else{
+        printf("Listas iguais");
+    }
+}
+
+void copyLists(StaticList *list1, StaticList *list2){
+    for(int i=list1->first; i < list1->last; i++){
+        addData(list2,list1->data[i]);
+    }
+}
+
+void copyListsInverted(StaticList *list1, StaticList *list2){
+    for(int i=list1->last; i > list1->first; i--){
+        addData(list2,list1->data[i-1]);
+    }
+}
+
+void interleaveData(StaticList *list1, StaticList *list2, StaticList *list3){
+    int i = list1->first, j = list2->first, k = VetorStart;
+    while (i < list1->last && j < list2->last){
+        if(list1->data[i].value < list2->data[j].value){
+            addData(list3,list1->data[i]);
+            i++;
+        }else{
+            addData(list3,list2->data[j]);
+        }
+    }
+
+    while (i < list1->last)
+    {
+        addData(list3,list1->data[i]);
+        i++;
+    }
+
+    while (j< list1->last)
+    {
+        addData(list3,list2->data[j]);
+    }
+    
+}
+   
+
+void deleteOcorrences(StaticList *List, Data data_to_delete){
+    int i = List->first, j = List->first;
+    while (i<List->last)
+    {
+        if(List->data[i].value != data_to_delete.value){
+            List->data[j] = List->data[i];
+            j++;
+        }
+        i++;
+    }
+    List->last = j;
+}
+
+int main(){
+    StaticList List1;
+    Data data1, data2, data3, data4, data5;
+    StartList(&List1);
+    data1.value = 1;
+    data2.value = 2;
+    data3.value = 3;
+    data4.value = 5;
+    data5.value = 4;
+    addData(&List1, data1);
+    addData(&List1, data1);
+    addData(&List1, data4);
+    addData(&List1, data1);
+    addData(&List1, data1);
+    addData(&List1, data2);
+    showList(&List1);
+    deleteOcorrences(&List1, data1);
+    showList(&List1);
+    return 0;
+}
---
+++ 4-ListaEstatica/readme.md
@@ -0,0 +1,10 @@
+1. Implemente uma função para inserir um elemento numa lista L em uma posição pos.
+2. Dadas duas listas L1 e L2, que possam estar desordenadas e contendo elementos
+repetidos:
+2.1. Verifique se L1 está ordenada ou não (a ordem pode ser crescente ou decrescente);
+2.2. Faça uma função que verifique se L1 é igual a L2;
+2.3. Faça uma cópia da lista L1 em uma outra lista L2;
+2.4. Inverta L1 colocando o resultado em L2;
+2.5. Intercale L1 com a lista L2, gerando a lista L3. Considere que L1, L2 são ordenadas.
+2.6. Elimine de L1 todas as ocorrências de um elemento dado, considere que L1 está
+ordenada crescentemente;    
